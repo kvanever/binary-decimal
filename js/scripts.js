07b36bf3a201fe40e3c9f49d0051adb8d21d7114
Kevin VanEvery and Matthew Giem <kevin@>
2016-06-08 16:13:29 -0700

    adds function to evaluate binary into decimal

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,37 @@
 //front-end
 
 $(function () {
-  $("form.binary-form").submit(function(event) {
+  $("form").submit(function(event) {
     event.preventDefault();
-    var input = parseInt($("input#binary-input").val());
+    debugger;
+    var input = ($("input#binary-input").val());
+    // if (!isBinary(input) === true) {
+    //   alert("Please enter a positive binary integer");
+    //   $("input").addClass("has-error");
+    // }
+    // else {
+      var output = binaryToDecimal(input);
+    // }
+    $(".output").text(output);
   })
 })
+
+//business logic
+
+var isBinary = function(integer) {
+  return (/^[-e23456789]$/i).test(integer);
+}
+
+var binaryToDecimal = function(integer) {
+  debugger;
+  var array = integer.split('');
+  var decimal = 0;
+  var reverseArray = array.reverse();
+  for (i = 0; i < reverseArray.length; i++) {
+    reverseArray[i] = parseInt(reverseArray[i]);
+    if (reverseArray[i]) {
+      decimal += Math.pow(2, i)
+    }
+  }
+  return decimal;
+}
